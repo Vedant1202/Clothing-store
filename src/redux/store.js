96c343f7877c338b29dd1ba1267d17e185bc73c4
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2020-06-27 01:26:05 +0530

    Changed logger to dev only

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ const { default: logger } = require('redux-logger');
 const { createStore, applyMiddleware } = require('redux');
 const { default: rootReducer } = require('./root.reducer');
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
